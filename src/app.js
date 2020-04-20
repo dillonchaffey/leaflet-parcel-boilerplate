51d54e2af17fb1060a266ec808d5d4a41e8bed30
DC <dillonchaffey@gmail.com>
2020-04-20 03:01:48 -0300

    #

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ import './scss/app.scss';
 const app = async () => {
   document.getElementById('header').innerHTML = Header();
   // document.getElementById('user').innerHTML = await User();
-  LeafletMap();
+  LeafletMap('leaflet-map-container');
 };
 
 
--- src/components/LeafletMap.js
+++ src/components/LeafletMap.js
@@ -5,18 +5,23 @@ import '../../node_modules/leaflet/dist/images/marker-shadow.png';
 
 import '../../node_modules/leaflet/dist/leaflet';
 
-const LeafletMap = () => {
-  var mymap = L.map('map').setView([51.505, -0.09], 13);
+const LeafletMap = (mapClassName) => {
+  let mapContainerEls = document.getElementsByClassName(mapClassName);
 
-  L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
-    maxZoom: 18,
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
-      '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
-      'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    id: 'mapbox/streets-v11',
-    tileSize: 512,
-    zoomOffset: -1
-  }).addTo(mymap);
+  for (let containerEl of mapContainerEls) {
+    let mymap = L.map(containerEl).setView([51.505, -0.09], 13);
+
+    L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+      maxZoom: 18,
+      attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
+        '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+        'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+      id: 'mapbox/streets-v11',
+      tileSize: 512,
+      zoomOffset: -1
+    }).addTo(mymap);
+  }
 }
+
 
 export default LeafletMap;
